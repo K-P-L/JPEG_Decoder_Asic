87a031e7c5b79a46bc7604d5b8894147a8716c69
Kaustubh Prashant Lad <kaulad@uw.edu>
2025-04-28 14:50:39 -0700

    Deleting Build and Simulation Files

--- build/par-rundir/floorplan.tcl
+++
@@ -1,5 +0,0 @@
-create_floorplan -core_margins_by die -flip f -die_size_by_io_height max -site unithd -die_size { 500 500 0 0 0 0 }
-# Floorplan automatically generated from HAMMER
-# Floorplan manually specified from HAMMER
- create_fence -name guts/link_downstream -area  40 40 340 310 
- create_fence -name guts/link_upstream -area 300 250 450 450 
