1b8a466d0e7fe722ef1f8ffbe9f69fbc5b131759
Vidhaya Datta Reddy Metta <vmetta@linux-lab-045.ece.uw.edu>
2025-04-28 04:10:29 -0700

    Changes made to jpeg_mcu_proc module, yet to change instatiatioins from jpeg_input

--- v/jpeg_mcu_proc.v
+++ v/jpeg_mcu_proc.v
@@ -32,58 +32,64 @@
 module jpeg_mcu_proc
 (
     // Inputs
-     input           clk_i
-    ,input           rst_i
-    ,input           img_start_i
-    ,input           img_end_i
-    ,input  [ 15:0]  img_width_i
-    ,input  [ 15:0]  img_height_i
-    ,input  [  1:0]  img_mode_i
-    ,input           inport_valid_i
-    ,input  [ 31:0]  inport_data_i
-    ,input           inport_last_i
-    ,input           lookup_valid_i
-    ,input  [  4:0]  lookup_width_i
-    ,input  [  7:0]  lookup_value_i
-    ,input           outport_blk_space_i
+     input  logic           clk_i
+    ,input  logic           rst_i
+    ,input  logic           img_start_i
+    ,input  logic           img_end_i
+    ,input  logic [ 15:0]   img_width_i
+    ,input  logic [ 15:0]   img_height_i
+    ,input  logic [  1:0]   img_mode_i
+    ,input  logic           inport_valid_i
+    ,input  logic [ 31:0]   inport_data_i
+    ,input  logic           inport_last_i
+    ,input  logic           lookup_valid_i
+    ,input  logic [  4:0]   lookup_width_i
+    ,input  logic [  7:0]   lookup_value_i
+    ,input  logic           yumi_i              // YUMI input signal (replaces outport_blk_space_i)
 
     // Outputs
-    ,output [  5:0]  inport_pop_o
-    ,output          lookup_req_o
-    ,output [  1:0]  lookup_table_o
-    ,output [ 15:0]  lookup_input_o
-    ,output          outport_valid_o
-    ,output [ 15:0]  outport_data_o
-    ,output [  5:0]  outport_idx_o
-    ,output [ 31:0]  outport_id_o
-    ,output          outport_eob_o
+    ,output logic [  5:0]   inport_pop_o        // Special signal - indicates bits to consume
+    ,output logic           lookup_req_o        // Request to lookup table
+    ,output logic [  1:0]   lookup_table_o
+    ,output logic [ 15:0]   lookup_input_o
+    ,output logic           v_o                 // Valid output signal (replaces outport_valid_o)
+    ,output logic [ 15:0]   outport_data_o
+    ,output logic [  5:0]   outport_idx_o
+    ,output logic [ 31:0]   outport_id_o
+    ,output logic           outport_eob_o
 );
 
-
-
 //-----------------------------------------------------------------
-// Block Type (Y, Cb, Cr)
+// Block Type (Y, Cb, Cr) and Huffman Tables
 //-----------------------------------------------------------------
-wire start_block_w;
-wire next_block_w;
-wire end_of_image_w;
+// Image format constants
+typedef enum logic [1:0] {
+    JPEG_MONOCHROME  = 2'd0,
+    JPEG_YCBCR_444   = 2'd1,
+    JPEG_YCBCR_420   = 2'd2,
+    JPEG_UNSUPPORTED = 2'd3
+} jpeg_format_e;
 
-localparam JPEG_MONOCHROME  = 2'd0;
-localparam JPEG_YCBCR_444   = 2'd1;
-localparam JPEG_YCBCR_420   = 2'd2;
-localparam JPEG_UNSUPPORTED = 2'd3;
+// Block type constants
+typedef enum logic [1:0] {
+    BLOCK_Y          = 2'd0,
+    BLOCK_CB         = 2'd1,
+    BLOCK_CR         = 2'd2,
+    BLOCK_EOF        = 2'd3
+} block_type_e;
 
-localparam BLOCK_Y          = 2'd0;
-localparam BLOCK_CB         = 2'd1;
-localparam BLOCK_CR         = 2'd2;
-localparam BLOCK_EOF        = 2'd3;
+// Huffman table indices
+typedef enum logic [1:0] {
+    DHT_TABLE_Y_DC_IDX  = 2'd0,
+    DHT_TABLE_Y_AC_IDX  = 2'd1,
+    DHT_TABLE_CX_DC_IDX = 2'd2,
+    DHT_TABLE_CX_AC_IDX = 2'd3
+} dht_table_e;
 
-wire [1:0] block_type_w;
-
-localparam DHT_TABLE_Y_DC_IDX  = 2'd0;
-localparam DHT_TABLE_Y_AC_IDX  = 2'd1;
-localparam DHT_TABLE_CX_DC_IDX = 2'd2;
-localparam DHT_TABLE_CX_AC_IDX = 2'd3;
+logic start_block_w;
+logic next_block_w;
+logic end_of_image_w;
+logic [1:0] block_type_w;
 
 jpeg_mcu_id
 u_id
@@ -108,30 +114,31 @@ u_id
 //-----------------------------------------------------------------
 // FSM
 //-----------------------------------------------------------------
-localparam STATE_W           = 5;
-localparam STATE_IDLE        = 5'd0;
-localparam STATE_FETCH_WORD  = 5'd1;
-localparam STATE_HUFF_LOOKUP = 5'd2;
-localparam STATE_OUTPUT      = 5'd3;
-localparam STATE_EOB         = 5'd4;
-localparam STATE_EOF         = 5'd5;
+typedef enum logic [4:0] {
+    STATE_IDLE        = 5'd0,
+    STATE_FETCH_WORD  = 5'd1,
+    STATE_HUFF_LOOKUP = 5'd2,
+    STATE_OUTPUT      = 5'd3,
+    STATE_EOB         = 5'd4,
+    STATE_EOF         = 5'd5
+} state_e;
 
-reg [STATE_W-1:0] state_q;
-reg [STATE_W-1:0] next_state_r;
+state_e state_q;
+state_e next_state_r;
 
-reg [7:0]         code_bits_q;
-reg [7:0]         coeff_idx_q;
+logic [7:0] code_bits_q;
+logic [7:0] coeff_idx_q;
 
-always @ *
+always_comb
 begin
     next_state_r = state_q;
 
     case (state_q)
     STATE_IDLE:
     begin
-        if (end_of_image_w && outport_blk_space_i)
+        if (end_of_image_w && yumi_i)
             next_state_r = STATE_EOF;
-        else if (inport_valid_i && outport_blk_space_i)
+        else if (inport_valid_i && yumi_i)
             next_state_r = STATE_FETCH_WORD;
     end
     STATE_FETCH_WORD:
@@ -169,75 +176,89 @@ end
 assign start_block_w = (state_q == STATE_IDLE && next_state_r != STATE_IDLE);
 assign next_block_w  = (state_q == STATE_EOB);
 
-always @ (posedge clk_i )
-if (rst_i)
-    state_q <= STATE_IDLE;
-else
-    state_q <= next_state_r;
+always_ff @ (posedge clk_i)
+begin
+    if (rst_i)
+        state_q <= STATE_IDLE;
+    else
+        state_q <= next_state_r;
+end
 
-reg first_q;
+logic first_q;
 
-always @ (posedge clk_i )
-if (rst_i)
-    first_q <= 1'b1;
-else if (state_q == STATE_IDLE)
-    first_q <= 1'b1;
-else if (state_q == STATE_OUTPUT)
-    first_q <= 1'b0;
+always_ff @ (posedge clk_i)
+begin
+    if (rst_i)
+        first_q <= 1'b1;
+    else if (state_q == STATE_IDLE)
+        first_q <= 1'b1;
+    else if (state_q == STATE_OUTPUT)
+        first_q <= 1'b0;
+end
 
 //-----------------------------------------------------------------
 // Huffman code lookup stash
 //-----------------------------------------------------------------
-reg [7:0] code_q;
+logic [7:0] code_q;
 
-always @ (posedge clk_i )
-if (rst_i)
-    code_q <= 8'b0;
-else if (state_q == STATE_HUFF_LOOKUP && lookup_valid_i)
-    code_q <= lookup_value_i;
+always_ff @ (posedge clk_i)
+begin
+    if (rst_i)
+        code_q <= 8'b0;
+    else if (state_q == STATE_HUFF_LOOKUP && lookup_valid_i)
+        code_q <= lookup_value_i;
+end
 
 //-----------------------------------------------------------------
 // code[3:0] = width of symbol
 //-----------------------------------------------------------------
-always @ (posedge clk_i )
-if (rst_i)
-    code_bits_q <= 8'b0;
-else if (state_q == STATE_HUFF_LOOKUP && lookup_valid_i)
-    code_bits_q <= {4'b0, lookup_value_i[3:0]};
+always_ff @ (posedge clk_i)
+begin
+    if (rst_i)
+        code_bits_q <= 8'b0;
+    else if (state_q == STATE_HUFF_LOOKUP && lookup_valid_i)
+        code_bits_q <= {4'b0, lookup_value_i[3:0]};
+end
 
 //-----------------------------------------------------------------
 // Lookup width flops
 //-----------------------------------------------------------------
-reg [4:0] lookup_width_q;
+logic [4:0] lookup_width_q;
 
-always @ (posedge clk_i )
-if (rst_i)
-    lookup_width_q <= 5'b0;
-else if (state_q == STATE_HUFF_LOOKUP && lookup_valid_i)
-    lookup_width_q <= lookup_width_i;
+always_ff @ (posedge clk_i)
+begin
+    if (rst_i)
+        lookup_width_q <= 5'b0;
+    else if (state_q == STATE_HUFF_LOOKUP && lookup_valid_i)
+        lookup_width_q <= lookup_width_i;
+end
 
 //-----------------------------------------------------------------
 // Data for coefficient (remainder from Huffman lookup)
 //-----------------------------------------------------------------
-reg [15:0] input_data_q;
+logic [15:0] input_data_q;
+logic [31:0] input_shift_w;
 
-wire [31:0] input_shift_w = inport_data_i >> (5'd16 - lookup_width_i);
+assign input_shift_w = inport_data_i >> (5'd16 - lookup_width_i);
 
-always @ (posedge clk_i )
-if (rst_i)
-    input_data_q <= 16'b0;
-// Use remaining data for actual coeffecient
-else if (state_q == STATE_HUFF_LOOKUP && lookup_valid_i)
-    input_data_q <= input_shift_w[15:0];
+always_ff @ (posedge clk_i)
+begin
+    if (rst_i)
+        input_data_q <= 16'b0;
+    // Use remaining data for actual coeffecient
+    else if (state_q == STATE_HUFF_LOOKUP && lookup_valid_i)
+        input_data_q <= input_shift_w[15:0];
+end
 
 //-----------------------------------------------------------------
 // Bit buffer pop
 //-----------------------------------------------------------------
-reg [5:0]  pop_bits_r;
+logic [5:0] pop_bits_r;
+logic [4:0] coef_bits_w;
 
-wire [4:0] coef_bits_w = {1'b0, code_q[3:0]};
+assign coef_bits_w = {1'b0, code_q[3:0]};
 
-always @ *
+always_comb
 begin
     pop_bits_r = 6'b0;
 
@@ -253,7 +274,7 @@ begin
         else
             pop_bits_r = {1'b0, lookup_width_q} + coef_bits_w;
     end
-    default : ;
+    default: ;
     endcase
 end
 
@@ -261,8 +282,9 @@ assign lookup_req_o   = (state_q == STATE_FETCH_WORD) & inport_valid_i;
 assign lookup_input_o = inport_data_i[31:16];
 assign inport_pop_o   = pop_bits_r;
 
-reg [1:0] lookup_table_r;
-always @ *
+logic [1:0] lookup_table_r;
+
+always_comb
 begin
     lookup_table_r = DHT_TABLE_Y_DC_IDX;
 
@@ -281,15 +303,17 @@ begin
             lookup_table_r = DHT_TABLE_CX_AC_IDX;
     end
 end
+
 assign lookup_table_o = lookup_table_r;
 
 //-----------------------------------------------------------------------------
 // decode_number: Extract number from code / width
 //-----------------------------------------------------------------------------
-function [15:0] decode_number;
-    input [15:0] w;
-    input [4:0]  bits;
-    reg signed [15:0] code;
+function logic [15:0] decode_number;
+    input logic [15:0] w;
+    input logic [4:0]  bits;
+    
+    logic signed [15:0] code;
 begin
     code = w;
 
@@ -297,6 +321,7 @@ begin
     begin
         code = (code | ((~0) << bits)) + 1;
     end
+    
     decode_number = code;
 end
 endfunction
@@ -304,35 +329,34 @@ endfunction
 //-----------------------------------------------------------------
 // Previous DC coeffecient
 //-----------------------------------------------------------------
-wire [1:0] comp_idx_w = block_type_w;
+logic [1:0] comp_idx_w;
+logic [15:0] prev_dc_coeff_q[0:3];
+logic [15:0] dc_coeff_q;
 
-reg [15:0] prev_dc_coeff_q[3:0];
-reg [15:0] dc_coeff_q;
+assign comp_idx_w = block_type_w;
 
-always @ (posedge clk_i )
-if (rst_i)
+always_ff @ (posedge clk_i)
 begin
-    prev_dc_coeff_q[0] <= 16'b0;
-    prev_dc_coeff_q[1] <= 16'b0;
-    prev_dc_coeff_q[2] <= 16'b0;
-    prev_dc_coeff_q[3] <= 16'b0; // X
+    if (rst_i)
+    begin
+        for (int i = 0; i < 4; i = i + 1)
+            prev_dc_coeff_q[i] <= 16'b0;
+    end
+    else if (img_start_i)
+    begin
+        for (int i = 0; i < 4; i = i + 1)
+            prev_dc_coeff_q[i] <= 16'b0;
+    end
+    else if (state_q == STATE_EOB)
+        prev_dc_coeff_q[comp_idx_w] <= dc_coeff_q;
 end
-else if (img_start_i)
-begin
-    prev_dc_coeff_q[0] <= 16'b0;
-    prev_dc_coeff_q[1] <= 16'b0;
-    prev_dc_coeff_q[2] <= 16'b0;
-    prev_dc_coeff_q[3] <= 16'b0; // X
-end
-else if (state_q == STATE_EOB)
-    prev_dc_coeff_q[comp_idx_w] <= dc_coeff_q;
 
 //-----------------------------------------------------------------
 // coeff
 //-----------------------------------------------------------------
-reg [15:0] coeff_r;
+logic [15:0] coeff_r;
 
-always @ *
+always_comb
 begin
     if (coeff_idx_q == 8'b0)
         coeff_r = decode_number(input_data_q >> (16 - coef_bits_w), coef_bits_w) + prev_dc_coeff_q[comp_idx_w];
@@ -343,111 +367,78 @@ end
 //-----------------------------------------------------------------
 // dc_coeff
 //-----------------------------------------------------------------
-always @ (posedge clk_i )
-if (rst_i)
-    dc_coeff_q <= 16'b0;
-else if (state_q == STATE_OUTPUT && coeff_idx_q == 8'b0)
-    dc_coeff_q <= coeff_r;
+always_ff @ (posedge clk_i)
+begin
+    if (rst_i)
+        dc_coeff_q <= 16'b0;
+    else if (state_q == STATE_OUTPUT && coeff_idx_q == 8'b0)
+        dc_coeff_q <= coeff_r;
+end
 
 //-----------------------------------------------------------------
 // DC / AC coeff
 //-----------------------------------------------------------------
-reg [15:0] coeff_q;
+logic [15:0] coeff_q;
 
-always @ (posedge clk_i )
-if (rst_i)
-    coeff_q <= 16'b0;
-else if (state_q == STATE_OUTPUT)
-    coeff_q <= coeff_r;
+always_ff @ (posedge clk_i)
+begin
+    if (rst_i)
+        coeff_q <= 16'b0;
+    else if (state_q == STATE_OUTPUT)
+        coeff_q <= coeff_r;
+end
 
 //-----------------------------------------------------------------
 // Coeffecient index
 //-----------------------------------------------------------------
-always @ (posedge clk_i )
-if (rst_i)
-    coeff_idx_q <= 8'b0;
-else if (state_q == STATE_EOB || img_start_i)
-    coeff_idx_q <= 8'b0;
-else if (state_q == STATE_FETCH_WORD && !first_q && inport_valid_i)
-    coeff_idx_q <= coeff_idx_q + 8'd1;
-else if (state_q == STATE_OUTPUT)
+always_ff @ (posedge clk_i)
 begin
-    // DC
-    if (coeff_idx_q == 8'b0)
-        ;
-    // AC
-    else
+    if (rst_i)
+        coeff_idx_q <= 8'b0;
+    else if (state_q == STATE_EOB || img_start_i)
+        coeff_idx_q <= 8'b0;
+    else if (state_q == STATE_FETCH_WORD && !first_q && inport_valid_i)
+        coeff_idx_q <= coeff_idx_q + 8'd1;
+    else if (state_q == STATE_OUTPUT)
     begin
-        // End of block
-        if (code_q == 8'b0)
-            coeff_idx_q <= 8'd64;
-        // ZRL - 16 zeros
-        else if (code_q == 8'hF0)
-            coeff_idx_q <= coeff_idx_q + 8'd15;
-        // RLE number zeros (0 - 15)
+        // DC
+        if (coeff_idx_q == 8'b0)
+            ;  // No change
+        // AC
         else
-            coeff_idx_q <= coeff_idx_q + {4'b0, code_q[7:4]};
+        begin
+            // End of block
+            if (code_q == 8'b0)
+                coeff_idx_q <= 8'd64;
+            // ZRL - 16 zeros
+            else if (code_q == 8'hF0)
+                coeff_idx_q <= coeff_idx_q + 8'd15;
+            // RLE number zeros (0 - 15)
+            else
+                coeff_idx_q <= coeff_idx_q + {4'b0, code_q[7:4]};
+        end
     end
 end
 
 //-----------------------------------------------------------------
 // Output push
 //-----------------------------------------------------------------
-reg push_q;
+logic push_q;
 
-always @ (posedge clk_i )
-if (rst_i)
-    push_q <= 1'b0;
-else if (state_q == STATE_OUTPUT || state_q == STATE_EOF)
-    push_q <= 1'b1;
-else
-    push_q <= 1'b0;
+always_ff @ (posedge clk_i)
+begin
+    if (rst_i)
+        push_q <= 1'b0;
+    else if (state_q == STATE_OUTPUT || state_q == STATE_EOF)
+        push_q <= 1'b1;
+    else
+        push_q <= 1'b0;
+end
 
-assign outport_valid_o = push_q && (coeff_idx_q < 8'd64);
-assign outport_data_o  = coeff_q;
-assign outport_idx_o   = coeff_idx_q[5:0];
-assign outport_eob_o   = (state_q == STATE_EOB) || 
-                         (state_q == STATE_EOF && push_q);
-
-`ifdef verilator
-function get_valid; /*verilator public*/
-begin
-    get_valid = outport_valid_o && block_type_w != BLOCK_EOF;
-end
-endfunction
-function [5:0] get_sample_idx; /*verilator public*/
-begin
-    get_sample_idx = outport_idx_o;
-end
-endfunction
-function [15:0] get_sample; /*verilator public*/
-begin
-    get_sample = outport_data_o;
-end
-endfunction
-
-function [5:0] get_bitbuffer_pop; /*verilator public*/
-begin
-    get_bitbuffer_pop = inport_pop_o;
-end
-endfunction
-
-function get_dht_valid; /*verilator public*/
-begin
-    get_dht_valid = lookup_valid_i && (state_q == STATE_HUFF_LOOKUP);
-end
-endfunction
-function [4:0] get_dht_width; /*verilator public*/
-begin
-    get_dht_width = lookup_width_i;
-end
-endfunction
-function [7:0] get_dht_value; /*verilator public*/
-begin
-    get_dht_value = lookup_value_i;
-end
-endfunction
-`endif
-
+assign v_o = push_q && (coeff_idx_q < 8'd64);
+assign outport_data_o = coeff_q;
+assign outport_idx_o = coeff_idx_q[5:0];
+assign outport_eob_o = (state_q == STATE_EOB) || 
+                       (state_q == STATE_EOF && push_q);
 
 endmodule
