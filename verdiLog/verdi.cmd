030ff285eafc0e8eea25231c0042a3024fb85817
Vidhaya Datta Reddy Metta <vmetta@linux-lab-045.ece.uw.edu>
2025-05-05 02:46:04 -0700

    Ignore VerdiLog added to git ignore

--- verdiLog/verdi.cmd
+++ verdiLog/verdi.cmd
@@ -9,73 +9,29 @@ wvCreateWindow
 srcHBSelect "jpeg_core_tb.DUT" -win $_nTrace1
 srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT" -delim "."
 srcHBSelect "jpeg_core_tb.DUT" -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT.u_jpeg_input" -delim "."
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT.u_jpeg_input" -delim "."
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcHBSelect "jpeg_core_tb" -win $_nTrace1
 srcHBSelect "jpeg_core_tb.DUT" -win $_nTrace1
 srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT" -delim "."
 srcHBSelect "jpeg_core_tb.DUT" -win $_nTrace1
-srcHBSelect "jpeg_core_tb" -win $_nTrace1
 srcSignalView -on
-srcSignalViewSelect "jpeg_core_tb.DUT.inport_valid_i"
+srcSignalViewSelect "jpeg_core_tb.DUT.clk_i"
 srcSignalViewAddSelectedToWave -win $_nTrace1
-srcSignalViewSelect "jpeg_core_tb.DUT.inport_strb_i\[3:0\]"
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
 srcSignalViewAddSelectedToWave -win $_nTrace1
-srcSignalViewSelect "jpeg_core_tb.DUT.inport_valid_i"
+wvSelectSignal -win $_nWave2 {( "G1" 1 )} 
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
 srcSignalViewAddSelectedToWave -win $_nTrace1
-srcSignalViewSelect "jpeg_core_tb.DUT.dqt_cfg_accept_w"
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
 srcSignalViewAddSelectedToWave -win $_nTrace1
-srcSignalViewSelect "jpeg_core_tb.DUT.lookup_table_w\[1:0\]"
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
 srcSignalViewAddSelectedToWave -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_dht" -win $_nTrace1
-srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT.u_jpeg_dht" -delim "."
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_dht" -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_dqt" -win $_nTrace1
-srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT.u_jpeg_dqt" -delim "."
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_dqt" -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_dqt" -win $_nTrace1
-srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT.u_jpeg_dqt" -delim "."
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_dqt" -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT.u_jpeg_input" -delim "."
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_dht" -win $_nTrace1
-srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT.u_jpeg_dht" -delim "."
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_dht" -win $_nTrace1
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcSetScope -win $_nTrace1 "jpeg_core_tb.DUT.u_jpeg_input" -delim "."
-srcHBSelect "jpeg_core_tb.DUT.u_jpeg_input" -win $_nTrace1
-srcSignalViewSelect "jpeg_core_tb.DUT.u_jpeg_input.img_dqt_table_y_o\[1:0\]"
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
 srcSignalViewAddSelectedToWave -win $_nTrace1
-wvScrollDown -win $_nWave2 1
-wvScrollDown -win $_nWave2 1
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-srcSignalViewSelect "jpeg_core_tb.DUT.u_jpeg_input.dqt_cfg_data_o\[7:0\]"
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
 srcSignalViewAddSelectedToWave -win $_nTrace1
-wvScrollDown -win $_nWave2 1
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
+srcSignalViewAddSelectedToWave -win $_nTrace1
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
+srcSignalViewSelect "jpeg_core_tb.DUT.rst_i"
